b01ea2a440f3f4c3cf44190277864f8d2b891d2d
Thomas Rhomberg <trhomberg3@gmail.com>
2021-05-05 14:00:24 -0500

    Initial commit

---
+++ scripts/app.js
@@ -0,0 +1,23 @@
+var current = 0;
+
+
+// Project carousel
+$(function(){
+    $('[data-navigation] li').on("click", function(){
+        $('.card-project:eq(' + current + ')').removeClass('card-project--active');
+
+        current = $(this).index();
+        $(this).addClass("is-active").siblings().removeClass('is-active');
+        $('.card-project:eq(' + current + ')').addClass('card-project--active');
+    });
+
+});
+
+// Contact form 
+$('.js-input').keyup(function(){
+    if( $(this).val() ) {
+        $(this).addClass('not-empty');
+    } else {
+        $(this).removeClass('not-empty');
+    }
+});
