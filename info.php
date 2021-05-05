f8bfcbc23d38c47b50060f327ff9a4f3ce1df05b
Thomas Rhomberg <trhomberg3@gmail.com>
2021-05-05 15:43:43 -0500

    initial

---
+++ info.php
@@ -0,0 +1,40 @@
+<?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+
+    require 'vendor/autoload.php';
+    include("info.php");
+
+    $mail = new PHPMailer(true);
+if(isset($_POST['submit'])){
+    try {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+        $mail->isSMTP();
+        $mail->Host =      "smtp.gmail.com";
+        $mail->SMTPAuth = true;
+        $mail->Username = 'trhomberg3@gmail.com';
+        $mail->Password = '2020Mustang!';
+        $mail->SMTPSecure = 'ssl';
+        $mail->Port = 465;
+
+        $mail->setFrom($email, $name);
+        $mail->addAddress('trhomberg3@gmail.com', 'Thomas');
+
+        $mail->wordwrap = 50;
+        $mail->isHTML(true);
+        $mail->Subject = "Message sent from contact form";
+        $mail->Body = $message;
+        
+        $mail->send();
+        header("Location: index.php");
+        exit();
+    } catch (Exception $e) {
+        echo "Message has not been sent.";
+        echo "Mailer error: " . $mail->ErrorInfo;
+    }
+}
+?>
